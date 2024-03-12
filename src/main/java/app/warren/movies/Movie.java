23637ecd306202a3d75a8874950c13482645e6eb
Warren Chan <warren.chan0113@gmail.com>
2024-03-12 18:15:36 -0400

    add Movie class

--- src/main/java/app/warren/movies/Movie.java
+++ src/main/java/app/warren/movies/Movie.java
@@ -1,2 +1,32 @@
-package app.warren.movies;public class Movie {
+package app.warren.movies;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import org.bson.types.ObjectId;
+import org.springframework.data.annotation.Id;
+import org.springframework.data.mongodb.core.mapping.Document;
+import org.springframework.data.mongodb.core.mapping.DocumentReference;
+
+import java.util.List;
+
+
+@Document(collection = "movies")
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+
+public class Movie {
+    @Id //unique identifier
+    private ObjectId id;
+    private String imbdId;
+    private String title;
+    private String releaseDate;
+    private String trailerLink;
+    private List<String> genres;
+    private String poster;
+    private List<String> backdrops;
+    @DocumentReference
+    private List<Review> reviews;
+
 }
---
+++ src/main/java/app/warren/movies/MovieController.java
@@ -0,0 +1,2 @@
+package app.warren.movies;public class MovieController {
+}
