69b566c826306d3d036e9a5af4ccedd391588462
Warren Chan <warren.chan0113@gmail.com>
2024-03-12 18:18:31 -0400

    add review class variables

--- src/main/java/app/warren/movies/Review.java
+++ src/main/java/app/warren/movies/Review.java
@@ -1,2 +1,17 @@
-package app.warren.movies;public class Review {
+package app.warren.movies;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import org.bson.types.ObjectId;
+import org.springframework.data.mongodb.core.mapping.Document;
+
+@Document(collection = "reviews")
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+
+public class Review {
+    private ObjectId id;
+    private String body;
 }
