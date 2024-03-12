dd4256c3a80504b8344e3f84b771e5eb3ab6d56c
Warren Chan <warren.chan0113@gmail.com>
2024-03-12 18:16:58 -0400

    add Movie Repository interface

--- src/main/java/app/warren/movies/MovieRepository.java
+++ src/main/java/app/warren/movies/MovieRepository.java
@@ -1,2 +1,9 @@
-package app.warren.movies;public interface MovieRepository {
+package app.warren.movies;
+
+import org.bson.types.ObjectId;
+import org.springframework.data.mongodb.repository.MongoRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface MovieRepository extends MongoRepository<Movie, ObjectId> {
 }
