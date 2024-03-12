5d29987144e607e24764a1dcac68424d9f81c09c
Warren Chan <warren.chan0113@gmail.com>
2024-03-12 18:17:59 -0400

    add get all and single movie db calls

--- src/main/java/app/warren/movies/MovieService.java
+++ src/main/java/app/warren/movies/MovieService.java
@@ -1,2 +1,22 @@
-package app.warren.movies;public class MovieService {
+package app.warren.movies;
+
+import org.bson.types.ObjectId;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import javax.swing.text.html.Option;
+import java.util.List;
+import java.util.Optional;
+
+@Service
+public class MovieService {
+    @Autowired // it allows Spring to automatically wire the required beans (dependencies) into your classes, eliminating the need for manual configuration.
+    private MovieRepository movieRepository;
+    public List<Movie> getAllMovies() {
+        return movieRepository.findAll();
+    }
+    public Optional<Movie> getSingleMovie(ObjectId id) {
+        return movieRepository.findById(id);
+
+    }
 }
