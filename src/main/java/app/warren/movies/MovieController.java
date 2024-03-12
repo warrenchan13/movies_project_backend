d8c8fbe5b729de6faf51951993b55d29b58a9820
Warren Chan <warren.chan0113@gmail.com>
2024-03-12 18:16:21 -0400

    add get routes for all movies and single movie

--- src/main/java/app/warren/movies/MovieController.java
+++ src/main/java/app/warren/movies/MovieController.java
@@ -1,2 +1,30 @@
-package app.warren.movies;public class MovieController {
+package app.warren.movies;
+
+import org.bson.types.ObjectId;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
+import java.util.Optional;
+
+@RestController // used to create RESTful web services using Spring MVC.
+//@RequestMapping("/api/movies")
+public class MovieController {
+    @Autowired // it allows Spring to automatically wire the required beans (dependencies) into your classes, eliminating the need for manual configuration.
+    private MovieService movieService;
+    @GetMapping("/api/movies")
+    public ResponseEntity<List<Movie>> getMovies() {
+        return new ResponseEntity<List<Movie>>(movieService.getAllMovies(), HttpStatus.OK);
+    }
+
+    @GetMapping("/api/movie/{id}")
+    public ResponseEntity<Optional<Movie>> getSingleMovie(@PathVariable ObjectId id) {
+        return new ResponseEntity<Optional<Movie>>(movieService.getSingleMovie(id), HttpStatus.OK);
+    }
+
 }
