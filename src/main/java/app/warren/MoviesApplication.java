d13e975fbc941508d433f7ddd293fc283a15e127
Warren Chan <warren.chan0113@gmail.com>
2024-03-15 14:36:04 -0400

    fix file structure

--- src/main/java/app/warren/MoviesApplication.java
+++ src/main/java/app/warren/MoviesApplication.java
@@ -2,6 +2,9 @@ package app.warren;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.CrossOrigin;
+
+@CrossOrigin(origins = "*")
 
 @SpringBootApplication
 public class MoviesApplication {
